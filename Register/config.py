de8ef688cd5599b96ead07586bcd0916f8b6e4f4
Jordan <rangeljordan090802@gmmail.com>
2025-02-11 12:48:26 -0600

    BackProductos

---
+++ Register/config.py
@@ -0,0 +1,18 @@
+# config.py
+import os
+
+DATABASE_CONFIG1 = {
+    "DRIVER": "{SQL Server}",
+    "SERVER": os.getenv("DB_SERVER1", "192.168.1.222\\GXSQLSTAFF"),
+    "DATABASE": os.getenv("DB_DATABASE1", "AuroFiscal"),
+    "UID": os.getenv("DB_USER1", "paginacad"),
+    "PWD": os.getenv("DB_PASSWORD1", "cadtoner")
+}
+
+DATABASE_DistProd = {
+    "DRIVER": "{SQL Server}",
+    "SERVER": os.getenv("DB_SERVER2", "DIR-JRANGEL\SERVERJR"),
+    "DATABASE": os.getenv("DB_DATABASE2", "DistBD"),
+    "UID": os.getenv("DB_USER2", "sa"),
+    "PWD": os.getenv("DB_PASSWORD2", "Jrangel01.")
+}
